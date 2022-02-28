1a307643bfa3be3f44ad8a5e6a070b43a514c24a
Adejar Junior <adejarjunior18@gmail.com>
2022-02-28 16:29:50 -0300

    chore: add new colors

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -2,5 +2,8 @@ export const theme = {
   BLACK: "#000000",
   WHITE: "#FFFFFF",
   PINK: "#B22E6F",
+  DARK_PINK: "#AB2680",
   GRAY: "#cfcece",
+  GRAY_DARK: "#333333",
+  GRAY_DARK_SECONDARY: "#999999",
 };
