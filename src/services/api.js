6ddfd35eabf5add625226457d7719b9baa897469
Adejar Junior <adejarjunior18@gmail.com>
2022-02-27 23:53:57 -0300

    feat: add API

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://books.ioasys.com.br/api/v1/auth/sign-in",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+export const enableToken = (access_token) => {
+  api.defaults.headers.common.Authorization = `Bearer ${access_token} `;
+};
+
+export default api;
