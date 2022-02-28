aca2ac6f245662bc705bf56b75fd6fc49ae5ddfc
Adejar Junior <adejarjunior18@gmail.com>
2022-02-27 23:57:24 -0300

    feat: add route books and refactor styles

--- src/view/Login/index.js
+++ src/view/Login/index.js
@@ -1,27 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import Auth from "../../controllers/Auth_Controller";
 
 import FormTextInput from "../../components/FormTextInput";
+import Error from "../../components/Error";
+import ImageBackGround from "../../components/ImageBackGround";
 
-import {
-  ContainerImg,
-  Container,
-  Logo,
-  Subtitulo,
-  ContainerTitulo,
-} from "./styles";
+import { Container, Logo, Subtitulo, ContainerTitulo } from "./styles";
+
+import BackgroundImage from "../../assets/image/jpg/backgroundImage_login.jpg";
 
 function Login() {
+  const [email, setEmail] = useState("desafio@ioasys.com.br");
+  const [password, setPassword] = useState("12341234");
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  async function entrar() {
+    setError(!error);
+    const response = await Auth.read(email, password);
+
+    if (response) {
+      navigate("/books");
+    } else {
+      setError(!error);
+    }
+  }
+
   return (
-    <ContainerImg>
+    <>
+      <ImageBackGround source={BackgroundImage} />
       <Container>
         <ContainerTitulo>
           <Logo />
           <Subtitulo>Books</Subtitulo>
         </ContainerTitulo>
-        <FormTextInput label="Email" placeholder="Email" />
-        <FormTextInput label="Senha" placeholder="Senha" />
+        <FormTextInput
+          label="Email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          type="email"
+        />
+        <FormTextInput
+          label="Senha"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          button
+          onClick={entrar}
+          textButton="Entrar"
+          type="password"
+        />
+        {error && <Error text={"Email e/ou senha incorretos."} />}
       </Container>
-    </ContainerImg>
+    </>
   );
 }
 
--- src/view/Login/styles.js
+++ src/view/Login/styles.js
@@ -2,16 +2,6 @@ import styled from "styled-components";
 
 import { theme } from "../../utils/Colors";
 
-export const ContainerImg = styled.div`
-  width: 100vw;
-  height: 100vh;
-  position: absolute;
-  background-image: url("/backgroundImage.jpg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-`;
-
 export const Container = styled.div`
   position: absolute;
   left: 8.42%;
@@ -39,10 +29,8 @@ export const Logo = styled.div`
   z-index: 2;
 `;
 
-export const Subtitulo = styled.h1`
+export const Subtitulo = styled.span`
   color: ${theme.WHITE};
-  margin-top: 20px;
-  font-family: Heebo;
   font-style: normal;
   font-weight: 300;
   font-size: 28px;
