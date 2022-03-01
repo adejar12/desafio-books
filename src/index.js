b14a1a7f11f162de8b640c2e2b5dab4d19eb1d90
Adejar Junior <adejarjunior18@gmail.com>
2022-03-01 18:59:43 -0300

    refactor: change locale router dom

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-import CssBaseline from "./index.ts";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./view/Login";
-import Books from "./view/Books";
+import App from "./App";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <CssBaseline />
-    <Routes>
-      <Route exact path="/" element={<Login />} />
-      <Route exact path="/books" element={<Books />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
