8f3f5ed4d5ed1685fac3ade4155b7b72c2876b1c
Adejar Junior <adejarjunior18@gmail.com>
2022-03-01 22:20:23 -0300

    fix: remove component

--- src/components/ImageBackGround/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Container } from "./styles";
-
-function ImageBackGround({ source }) {
-  return <Container source={source} />;
-}
-
-export default ImageBackGround;
